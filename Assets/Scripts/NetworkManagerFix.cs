321957f56e205eb81aa71f102955032d2d6e6a83
Carlos Miranda <carlosa_miranda@outlook.com>
2018-12-01 01:38:06 -0600

    tried so hard, and got so far, but in the end, it doesnt even matter

---
+++ Assets/Scripts/NetworkManagerFix.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManagerFix : MonoBehaviour {
+
+    private NetworkIdentity instance;
+    public Camera camera; 
+
+	// Use this for initialization
+	void Start () {
+        instance = this.GetComponent<NetworkIdentity>();
+        if (instance.isClient)
+        {
+            camera.gameObject.SetActive(true); 
+            Debug.Log(instance.name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
